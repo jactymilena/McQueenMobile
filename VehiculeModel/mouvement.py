be3fa9f004c7c56f0bc3957ce96ac930d1a3ed90
Lolomimi99 <laumil99@hotmail.com>
2023-03-02 13:28:40 -0500

    fix pr
    -enlever commentaires
    -ajout d'espaces

--- VehiculeModel/mouvement.py
+++ VehiculeModel/mouvement.py
@@ -1,15 +1,16 @@
 import bpy
 import math 
-#https://www.youtube.com/watch?v=ssHiWpVuxTk&ab_channel=Olav3DTutorials
-def move_forward(object, frame_num, starting_position, positions):
+
+
+def move_forward(object, frame_num, positions):
     for position in positions:
         bpy.context.scene.frame_set(frame_num)
         object.location = position
         object.keyframe_insert(data_path="location", index=-1)
         frame_num += 20 
 
-#https://www.youtube.com/watch?v=97CfID1UaOc
-def rotate(object,frame_num, rotations):
+
+def rotate(object, frame_num, rotations):
     for rotation in rotations:
         bpy.context.scene.frame_set(frame_num)
         object.rotation_euler = rotation
@@ -20,11 +21,11 @@ def rotate(object,frame_num, rotations):
 if __name__ == '__main__':
     ob = bpy.data.objects["car"]
     start_pos = (0,0,1.5)
-    positions = (0,0,1.5),(0,4,1.5),(0,6,1.5),(0,10,1.5),(0,5,1.5),(0,2,1.5)
+    positions = start_pos,(0,4,1.5),(0,6,1.5),(0,10,1.5),(0,5,1.5),(0,2,1.5)
     frame_num = 0
-    move_forward(ob, frame_num, start_pos, positions) 
+    move_forward(ob, frame_num, positions) 
     
     rotations = (0,0,0),(0,0,3),
     rotate(ob,120, rotations)
     po=(0,0,1.5),(0,-2,1.5)
-    move_forward(ob, 140, start_pos,po ) 
+    move_forward(ob, 140,po ) 
