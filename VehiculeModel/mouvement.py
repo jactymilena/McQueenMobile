86fad889e5ff0bd2692d4993256695919a54772e
Jacty <saej3101@usherbrooke.ca>
2023-03-08 13:09:28 -0500

    Added distance detection

--- VehiculeModel/mouvement.py
+++ VehiculeModel/mouvement.py
@@ -1,6 +1,6 @@
 import bpy
 import math 
-
+import numpy as np
 
 def frames_to_seconds(frames):
     # 24 frames per second
@@ -17,13 +17,18 @@ def move_forward(object, positions, start_frame = 0, frame_rate = 24):
         curr_frame += frame_rate 
         
 
-def rotate(object, frame_num, rotations):
+def rotate(object, rotations, start_frame = 0, frame_rate = 24):
+    curr_frame = start_frame
     for rotation in rotations:
-        bpy.context.scene.frame_set(frame_num)
+        bpy.context.scene.frame_set(curr_frame)
         object.rotation_euler = rotation
         object.keyframe_insert(data_path="rotation_euler", index=-1)
-        frame_num += 20 
+        frame_num += frame_rate 
 
+#def ultrasonic_sensor_distace(sensor_right, sensor_letf, 
+
+#def ultrasonic_sensor(car, sensors):
+    
 
 #def move(object, final_pos):
 #    if final_pos > object.location:
@@ -31,9 +36,12 @@ def rotate(object, frame_num, rotations):
 #    print("avance pas")
 
 
+#def check_ultrasonic_sensor(obstacle, sensor)
+ 
+
 
 if __name__ == '__main__':
-    obj = bpy.data.objects["car"]
+
 #    start_pos = (0,0,1.5)
 #    positions = start_pos,(0,4,1.5),(0,6,1.5),(0,10,1.5),(0,5,1.5),(0,2,1.5)
 #    frame_num = 0
@@ -44,25 +52,38 @@ if __name__ == '__main__':
 #    po=(0,0,1.5),(0,-2,1.5)
 #    move_forward(ob, 140,po ) 
     
-    pos = obj.location
-#    pos[1] += 10
-#    move(obj, pos)
+    car_obj = bpy.data.objects["car"]
+    obs_obj = bpy.data.objects["obstacle"]
+    sensor_right = bpy.data.objects["sensor_r"]
+    sensor_left = bpy.data.objects["sensor_l"]
     
     m = 1
     i = 1
+    sensor_range = 100
+    backwards_range = 50
     positions = []
-    positions
-    curr_pos = obj.location
+    curr_pos = car_obj.location
     positions.append(curr_pos[:])
+    
     print("before while")
+    
     while True:
         curr_pos[1] += m
+#        print(curr_pos - obs_obj.location)
+        
+        distance = (curr_pos - obs_obj.location).length
+        print(distance)
+        if distance <= sensor_range:
+            print("obstacle detected")
+        
         positions.append(curr_pos[:])
+        
+        
         i += 1
         
-        if(i > 100):
+        if i > 100:
             break
-    move_forward(obj, positions, 0, 2) 
-    print(positions)
+    move_forward(car_obj, positions, 0, 2) 
+#    print(positions)
         
         
