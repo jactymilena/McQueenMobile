6eee09a1c58e2ef7fe5a0c84f8653926540602d0
veronique <veronique.paquin2@usherbrooke.ca>
2023-04-06 13:36:20 -0400

    suit ligne

--- VehiculeModel/test/line_follow.py
+++ VehiculeModel/test/line_follow.py
@@ -21,7 +21,7 @@ def rotate(obj, axe, angle, keyframe):
 
 
 def turn(obj, axe1, angle, direction, keyframe, frame_rate, vit, rayon=12):
-    turn_direction = RIGHT if angle<0 else LEFT
+    turn_direction = RIGHT  if angle<0 else LEFT
 
     angle = np.abs(angle)
     dist = angle * rayon
@@ -67,9 +67,11 @@ def turn(obj, axe1, angle, direction, keyframe, frame_rate, vit, rayon=12):
     return position_array, rotation_array
 
 
-def move(obj, dist, axe, direction, keyframe):
+def move(obj, dist, axe, direction, angle,keyframe):
     new_pos = obj.location 
-    new_pos[axe] += dist*direction
+    #new_pos[axe] += dist*direction
+    new_pos[X_AXE] += dist*direction*np.cos(angle)
+    new_pos[Y_AXE] += dist*direction*np.sin(angle)
     obj.location = new_pos[:]
     obj.keyframe_insert(data_path="location", frame=keyframe)
 
@@ -101,13 +103,6 @@ def detectLine(sensor, trajectoire):
     sensor_bvh_tree = BVHTree.FromPolygons(sensor_verts, sensor_polys)
     traj_bvh_tree = BVHTree.FromPolygons(traj_verts, traj_polys)
     intersections = sensor_bvh_tree.overlap(traj_bvh_tree)
-#    
-#    if intersections:
-#        print('MESHES INTERSECTS')
-#        print(intersections)
-#    else:
-#        print('NO INTERSECTIONS')
-#    
     return intersections
 
 def line_status(sensor1, sensor2, sensor3, sensor4, sensor5, trajectoire):
@@ -125,20 +120,15 @@ def line_status(sensor1, sensor2, sensor3, sensor4, sensor5, trajectoire):
     return status
 
 
-def line_follow(off_track_count):
-    #sensor1_obj = bpy.data.objects["Cube"]
-    #sensor2_obj = bpy.data.objects["Cube.002"]
-    #sensor3_obj = bpy.data.objects["Cube.003"]
-    #sensor4_obj = bpy.data.objects["Cube.004"]
-    #sensor5_obj = bpy.data.objects["Cube.005"]
-#
-    #trajectoire = bpy.data.objects["Cube.001"
-    #call avancer
-    #while True:
+def line_follow(lt_status_now, off_track_count):
     step = 0
     turning_angle = 0
-    lt_status_now = line_status(sensor1_obj, sensor2_obj, sensor3_obj,sensor4_obj, sensor5_obj, trajectoire )
-    print(lt_status_now)
+    a_step = np.pi/60 # 3
+    b_step = np.pi/16  # 10
+    c_step = np.pi/8 # 30  degré
+    d_step = np.pi/4 #  45 degré
+    step_angle = 0 # axe y
+
     # Angle calculate
     if	lt_status_now == [0,0,1,0,0]:
         step = 0	
@@ -150,32 +140,31 @@ def line_follow(off_track_count):
         step = c_step
     elif lt_status_now == [1,0,0,0,0] or lt_status_now == [0,0,0,0,1]:
         step = d_step  
-        
+    
     # Direction calculate
     if	lt_status_now == [0,0,1,0,0]:
         off_track_count = 0
 
-    # turn right
-    elif lt_status_now in ([0,1,1,0,0],[0,1,0,0,0],[1,1,0,0,0],[1,0,0,0,0]):
+# turn right
+    elif lt_status_now in ([0,1,1,0,0],[0,1,0,0,0],[1,1,0,0,0],[1,0,0,0,0],[1,1,1,0,0]):
         off_track_count = 0
-        turning_angle = int(90 - step)
-    # turn left
-    elif lt_status_now in ([0,0,1,1,0],[0,0,0,1,0],[0,0,0,1,1],[0,0,0,0,1]):
+        turning_angle = -step
+# turn left
+    elif lt_status_now in ([0,0,1,1,0],[0,0,0,1,0],[0,0,0,1,1],[0,0,0,0,1],[0,0,1,1,1]):
         off_track_count = 0
-        turning_angle = int(90 + step)
+        turning_angle = step
     elif lt_status_now == [0,0,0,0,0]:
         off_track_count += 1
-        print(off_track_count)
-    step_angle = (turning_angle * np.pi *2 )/ 360
-    print(step_angle, "en rad")
-    #turn(car_obj, rotation_axe, step_angle, direction, keyframe, frame_rate, vit)
-    #rotate(car_obj, rotation_axe, step_angle, curr_frame)
+        turning_angle = 0
+    else :turning_angle = 0
+    #print(turning_angle, "en rad")
     
-    return off_track_count
+    return turning_angle
     
+#def is_turning(turning):
+ #   return !turning
 
 if __name__ == '__main__':
-    collection = bpy.data.collections['car']
     sensor1_obj = bpy.data.objects["sensor1"]
     sensor2_obj = bpy.data.objects["sensor2"]
     sensor3_obj = bpy.data.objects["sensor3"]
@@ -191,19 +180,14 @@ if __name__ == '__main__':
     
     move_dist = 2
     count = 0
-    front_axe = Y_AXE
-    rotation_axe = 2
+    front_axe = X_AXE
+    rotation_axe = Z_AXE
     frame_rate = 2
     curr_frame = 0
     direction = 1
-    step = 0
-    turning_angle = 0
-    a_step = 1
-    b_step = 5
-    c_step = 10 #degré
-    d_step = 25 #degré
-    step_angle = 0 # axe y
     off_track_count = 0
+    total_turning = 0
+   # turning = false
 
     while True:
         rotate(car_obj, rotation_axe, 0, curr_frame)
@@ -212,44 +196,44 @@ if __name__ == '__main__':
         print("curr_frame: ", curr_frame)
         lt_status_now = line_status(sensor1_obj, sensor2_obj, sensor3_obj,sensor4_obj, sensor5_obj, trajectoire )
         print(lt_status_now, " line status ")
-    # Angle calculate
-        if	lt_status_now == [0,0,1,0,0]:
-            step = 0	
-        elif lt_status_now == [0,1,1,0,0] or lt_status_now == [0,0,1,1,0]:
-            step = a_step
-        elif lt_status_now == [0,1,0,0,0] or lt_status_now == [0,0,0,1,0]:
-            step = b_step
-        elif lt_status_now == [1,1,0,0,0] or lt_status_now == [0,0,0,1,1]:
-            step = c_step
-        elif lt_status_now == [1,0,0,0,0] or lt_status_now == [0,0,0,0,1]:
-            step = d_step  
+#        print(turning_angle, "en rad")
+        turning_angle = line_follow(lt_status_now, 0)
         
-        # Direction calculate
-        if	lt_status_now == [0,0,1,0,0]:
-            off_track_count = 0
-
-    # turn right
-        elif lt_status_now in ([0,1,1,0,0],[0,1,0,0,0],[1,1,0,0,0],[1,0,0,0,0],[1,1,1,0,0]):
-            off_track_count = 0
-            turning_angle = -step
-    # turn left
-        elif lt_status_now in ([0,0,1,1,0],[0,0,0,1,0],[0,0,0,1,1],[0,0,0,0,1],[0,0,1,1,1]):
-            off_track_count = 0
-            turning_angle = step
-        elif lt_status_now == [0,0,0,0,0]:
-            off_track_count += 1
-            turning_angle = 0
-        step_angle = np.radians(turning_angle)#(turning_angle * np.pi *2 )/ 360
-        print(step_angle, "en rad")
-        turn(car_obj, rotation_axe, step_angle, direction, curr_frame, frame_rate,1.2375)
+        #if total_turning > np.pi/2:
+        #    front_axe = toggle_axe(front_axe)
+        #    direction = toggle_direction(direction)
+        #    total_turning = 0
+        position, rotation = turn(car_obj, front_axe, turning_angle, direction, curr_frame, frame_rate,1.23)
         #rotate(car_obj, rotation_axe, step_angle, curr_frame)
         
+        tmp_angle = turning_angle
+        for x in range(len(position)):
+                rotate(car_obj, rotation_axe, 0, curr_frame)
+                bpy.context.scene.frame_set(curr_frame)
+                lt_status_now = line_status(sensor1_obj, sensor2_obj, sensor3_obj,sensor4_obj, sensor5_obj, trajectoire )
+                turning_angle = line_follow(lt_status_now, 0)
+                print(lt_status_now, "status ", turning_angle, "en rad")
+                if tmp_angle != turning_angle:
+                    print("doit break", tmp_angle, "! = ", turning_angle)
+                    break
+                car_obj.location = position[x]
+                car_obj.rotation_euler = rotation[x]
+                car_obj.keyframe_insert(data_path="location", frame=curr_frame)
+                car_obj.keyframe_insert(data_path="rotation_euler", frame=curr_frame)
+                curr_frame += frame_rate
+                #print("curr_frame: ", curr_frame)
+                
+       # move(car_obj, 1.237, front_axe, direction, curr_frame)
+        
+        total_turning += turning_angle
+        print("total_turning = ", total_turning)
+        
         #if off_track_count == 0:
-        move(car_obj, move_dist, front_axe, direction, curr_frame)
+        move(car_obj, 1.23, front_axe, direction, total_turning,  curr_frame)
         curr_frame += frame_rate 
 
         count += 1
         
-        if count > 30:
+        if count > 60:
             # Trajectory end
             break
